de5bd2806406da48ab626d8bc976381e60e7fd80
patricia-diasr <patricia.d.rodrigues345@gmail.com>
2024-03-04 10:10:27 -0300

    Json server e categorias dinâmicas

--- src/components/project/ProjectsForm.js
+++ src/components/project/ProjectsForm.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from 'react';
+
 import styles from './ProjectForm.module.css';
+
 import Input from '../form/Input';
 import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
 
 function ProjectForm ({btnMessage}) {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect( () => {
+        fetch("http://localhost:5000/categories", {
+        method: "GET",
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+    .then( resp => resp.json() )
+    .then ( data => setCategories(data) )
+    .catch( err => console.log(err) );
+    }, [])
+
     return (
         <form className={styles.form}>
             <Input 
@@ -21,6 +39,7 @@ function ProjectForm ({btnMessage}) {
             <Select 
                 text="Selecione a categoria"
                 name="category_id"
+                options={categories}
             />
             <SubmitButton text={btnMessage} />
         </form>
